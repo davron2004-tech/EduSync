605c733677c6ee181b23875110f776a6e88c687a
Davron Abdukhakimov <davron0506150@gmail.com>
2024-02-13 17:45:55 +0500

    Initial Commit

---
+++ EduSync/EduSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduSyncApp.swift
+//  EduSync
+//
+//  Created by Davron Abdukhakimov on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EduSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
